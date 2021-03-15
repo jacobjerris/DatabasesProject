11b5fe69a04044d0a42fbe46345b6397e5628532
Luke Sanchez <57080084+lukesanchez@users.noreply.github.com>
2021-03-14 20:39:14 -0400

    added ingnoreCase and Admin prompt

--- SqlProject/src/App.java
+++ SqlProject/src/App.java
@@ -5,7 +5,7 @@ public class App {
         Scanner input = new Scanner(System.in);
         System.out.print("Are you a User or an Admin?\n");
         String user = input.nextLine();
-        if(user.contentEquals("User")) {
+        if(user.equalsIgnoreCase("User")) {
             System.out.println("What would you like to do?");
             System.out.println("1. Search\n2. Checkout\n3. Return\n4. Check Balance, Pay Balance and late fees\n5. Log Out\n");
             int userInput = input.nextInt();
@@ -20,7 +20,9 @@ public class App {
             } else {
                 System.out.println("Command not recognised");
             }
-        } else if (user.contentEquals("Admin")) {
+        } else if (user.equalsIgnoreCase("Admin")) {
+            System.out.println("What would you like to do?");
+            System.out.println("1. Locate Title\n2. Update inventory\n3. Check Balance\n4. Generate Reports\n5. Log Out\n");
             int userInput = input.nextInt();
             if(userInput == 1){
                 System.out.println("Locate Title here! ");
