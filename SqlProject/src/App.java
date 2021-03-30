23cb67f270fdc279b0bdd70002e228e2244e8354
Jacob Jerris <jacobjerris@icloud.com>
2021-03-30 16:51:51 -0400

    Created "authorName" function, this allows the user to input an author's first name, and it queries the database that I have locally and shows the first, last and middle initial of the author. Uses a prepared statement to stop SQL injection.

--- SqlProject/src/App.java
+++ SqlProject/src/App.java
@@ -11,9 +11,7 @@ public class App {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         //creating database connection
-        testQuery("root", "root");
-
-
+        //testQuery("root", "root");
         Scanner input = new Scanner(System.in);
             System.out.print("Are you a User or an Admin?\n");
             String user = input.nextLine();
@@ -31,70 +29,83 @@ public class App {
             input.close();
         }
     
-    public static void UserLoop(Scanner input) {
-    	System.out.println("What would you like to do?");
-        System.out.println("1. Search\n2. Checkout\n3. Return\n4. Check Balance, Pay Balance and late fees\n5. Log Out\n");
-        int userInput = input.nextInt();
-        
-        if(userInput == 1){
-            System.out.println("Enter your Book Title: ");
-            //query based on user input for a title, could use same query as in the "locate title" in admin panel
-       
-        } else if (userInput == 2) {
-            System.out.println("Checkout!");
-            //checkout could link to two options, rent or buy
-        
-        
-        } else if (userInput == 3) {
-            System.out.println("Books to return: ");
-            //query books that user has out as "rented", and give them price for each book?
-        
-        } else if (userInput == 4) {
+    public static void UserLoop(Scanner input) throws SQLException, ClassNotFoundException {
+        //while (true) {
             System.out.println("What would you like to do?");
-            System.out.println("1. Check Balance\n2.Pay Balance and late fees\n");
-            
-            userInput = input.nextInt();
-            
-            switch(userInput) {
-                case 1:
-                    System.out.println("Enter your customer number to view your balance here: ");
-                case 2:
-                    System.out.println("Enter your customer number to view the balance to be paid here: ");
-                    //balance query
-                    System.out.println("Enter your credit card credentials here: ");
-                    //once entered, the info should be cleared in their profile and display 0.00 when they submit
-            }
+            System.out.println("1. Search\n2. Checkout\n3. Return\n4. Check Balance, Pay Balance and late fees\n5. Log Out\n");
+            int userInput = input.nextInt();
+            if (userInput != 5) {
+                if (userInput == 1) {
+                    System.out.println("Enter your Book Title: ");
+                    String authorName = input.next();
 
-        } else {
-            System.out.println("Command not recognised");
+                    authorLookup("root", "root", authorName);
+                    //query based on user input for a title, could use same query as in the "locate title" in admin panel
+
+                } else if (userInput == 2) {
+                    System.out.println("Checkout: ");
+                    //checkout could link to two options, rent or buy
+
+
+                } else if (userInput == 3) {
+                    System.out.println("Books to return: ");
+                    //query books that user has out as "rented", and give them price for each book?
+
+                } else if (userInput == 4) {
+                    System.out.println("What would you like to do?");
+                    System.out.println("1. Check Balance\n2. Pay Balance and late fees\n");
+
+                    userInput = input.nextInt();
+
+                    switch (userInput) {
+                        case 1:
+                            System.out.println("Enter your customer number to view your balance here: ");
+                        case 2:
+                            System.out.println("Enter your customer number to view the balance to be paid here: ");
+                            //balance query
+                            System.out.println("Enter your credit card credentials here: ");
+                            //once entered, the info should be cleared in their profile and display 0.00 when they submit
+                    }
+                } else {
+                    System.out.println("Command not recognised");
+                }
+            //} //else {
+                //System.out.println("Good Bye!");
+                //break;
+            }
         }
-    }
+    //}
     
     public static void AdminLoop(Scanner input) {
-    	
-    	System.out.println("What would you like to do?");
-        System.out.println("1. Locate Title\n2. Update inventory\n3. Check Balance\n4. Generate Reports\n5. Log Out\n");
-        int userInput = input.nextInt();
-        
-        if(userInput == 1){
-            System.out.println("Please enter the name of the book you're trying to locate: ");
-            //query based on book name, show all details about book
-        
-        } else if (userInput == 2) {
-            System.out.println("What would you like to do?\n1. Add to inventory\n2. Delete title\n3. Update title");
-            //run query based on user input, prob a switch statement is needed, etc.
-        
-        } else if (userInput == 3) {
-            System.out.println("What would you like to do?\n1. Check user balance\n2. Apply late fee to user");
-            //depending on the option, could prob accomplish both tasks with one query
-        
-        } else if (userInput == 4) {
-            System.out.println("Generating Reports..");
-            //doesn't state for dates, but could ask for dates. or just print every record that we have, shouldn't
-            // be too much though
-        
-        } else {
-            System.out.println("Command not recognised");
+        while (true) {
+            System.out.println("What would you like to do?");
+            System.out.println("1. Locate Title\n2. Update inventory\n3. Check Balance\n4. Generate Reports\n5. Log Out\n");
+            int userInput = input.nextInt();
+            if (userInput != 5) {
+                if (userInput == 1) {
+                    System.out.println("Please enter the name of the book you're trying to locate: ");
+                    //query based on book name, show all details about book
+
+                } else if (userInput == 2) {
+                    System.out.println("What would you like to do?\n1. Add to inventory\n2. Delete title\n3. Update title");
+                    //run query based on user input, prob a switch statement is needed, etc.
+
+                } else if (userInput == 3) {
+                    System.out.println("What would you like to do?\n1. Check user balance\n2. Apply late fee to user");
+                    //depending on the option, could prob accomplish both tasks with one query
+
+                } else if (userInput == 4) {
+                    System.out.println("Generating Reports..");
+                    //doesn't state for dates, but could ask for dates. or just print every record that we have, shouldn't
+                    // be too much though
+
+                } else {
+                    System.out.println("Command not recognised");
+                }
+            } else {
+                System.out.println("Good Bye!");
+                break;
+            }
         }
     }
 
@@ -102,7 +113,7 @@ public class App {
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306", username, password);
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM local.author");
+        ResultSet rs = stmt.executeQuery("SELECT * FROM local.author ");
 
         while(rs.next()) {
             String s = rs.getString("FName");
@@ -111,5 +122,20 @@ public class App {
         }
     }
 
+    public static void authorLookup(String username, String password, String FName) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306", username, password);
+
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM local.author WHERE FName =?");
+        ps.setString(1, FName);
+
+        ResultSet rs = ps.executeQuery();
+        while(rs.next()) {
+            String s = rs.getString("FName");
+            String r = rs.getString("LName");
+            String q = rs.getString("MInitial");
+            System.out.print(s + " " + r + " " + q + " " + "\n");
+        }
+    }
 }
 
