89a6050dc65a3e13a1f978bec1b5e45de7995a0a
Luke Sanchez <57080084+lukesanchez@users.noreply.github.com>
2021-03-30 20:40:51 -0400

    Updated User and Admin loops to loop continually until user enters '5'

--- SqlProject/src/App.java
+++ SqlProject/src/App.java
@@ -24,87 +24,84 @@ public class App {
                 AdminLoop(input);
 
             }
-
-            //closing userInput bc it yells at me for mem issues
-            input.close();
+            input.close(); //closing userInput bc it yells at me for mem issues
         }
     
     public static void UserLoop(Scanner input) throws SQLException, ClassNotFoundException {
-        //while (true) {
-            System.out.println("What would you like to do?");
-            System.out.println("1. Search\n2. Checkout\n3. Return\n4. Check Balance, Pay Balance and late fees\n5. Log Out\n");
-            int userInput = input.nextInt();
-            if (userInput != 5) {
-                if (userInput == 1) {
-                    System.out.println("Enter your Book Title: ");
-                    String authorName = input.next();
+        int userInput = 0;
+        while (userInput != 5) { //loops through prompts until user enters 5.
+            System.out.println("\nWhat would you like to do?");
+            System.out.print("\t1. Search\n\t2. Checkout\n\t3. Return\n\t4. Check Balance, Pay Balance and late fees\n\t5. Log Out\nEnter Choice: ");
+            userInput = input.nextInt(); //collect user choice
+            if (userInput == 1) {
+                System.out.println("Enter your Book Title: ");
+                String authorName = input.next();
 
-                    authorLookup("root", "root", authorName);
-                    //query based on user input for a title, could use same query as in the "locate title" in admin panel
+                authorLookup("root", "root", authorName);
+                //query based on user input for a title, could use same query as in the "locate title" in admin panel
 
-                } else if (userInput == 2) {
-                    System.out.println("Checkout: ");
-                    //checkout could link to two options, rent or buy
+            } else if (userInput == 2) {
+                System.out.println("Checkout: ");
+                //checkout could link to two options, rent or buy
 
 
-                } else if (userInput == 3) {
-                    System.out.println("Books to return: ");
-                    //query books that user has out as "rented", and give them price for each book?
+            } else if (userInput == 3) {
+                System.out.println("Books to return: ");
+                //query books that user has out as "rented", and give them price for each book?
 
-                } else if (userInput == 4) {
-                    System.out.println("What would you like to do?");
-                    System.out.println("1. Check Balance\n2. Pay Balance and late fees\n");
+            } else if (userInput == 4) {
+                System.out.println("What would you like to do?");
+                System.out.print("\t1. Check Balance\n\t2. Pay Balance and late fees\nEnter Choice: ");
 
-                    userInput = input.nextInt();
+                userInput = input.nextInt();
 
-                    switch (userInput) {
-                        case 1:
-                            System.out.println("Enter your customer number to view your balance here: ");
-                        case 2:
-                            System.out.println("Enter your customer number to view the balance to be paid here: ");
-                            //balance query
-                            System.out.println("Enter your credit card credentials here: ");
-                            //once entered, the info should be cleared in their profile and display 0.00 when they submit
-                    }
-                } else {
-                    System.out.println("Command not recognised");
+                switch (userInput) {
+                    case 1:
+                        System.out.println("Enter your customer number to view your balance here: ");
+                    case 2:
+                        System.out.println("Enter your customer number to view the balance to be paid here: ");
+                        //balance query
+                        System.out.println("Enter your credit card credentials here: ");
+                        //once entered, the info should be cleared in their profile and display 0.00 when they submit
                 }
-            //} //else {
-                //System.out.println("Good Bye!");
-                //break;
+            }else if (userInput == 5){
+             System.out.println("Goodbye!");
+
+            } else {
+                System.out.println("Command not recognised");
             }
         }
-    //}
+    }
+
     
     public static void AdminLoop(Scanner input) {
-        while (true) {
-            System.out.println("What would you like to do?");
-            System.out.println("1. Locate Title\n2. Update inventory\n3. Check Balance\n4. Generate Reports\n5. Log Out\n");
-            int userInput = input.nextInt();
-            if (userInput != 5) {
-                if (userInput == 1) {
-                    System.out.println("Please enter the name of the book you're trying to locate: ");
-                    //query based on book name, show all details about book
+        int userInput = 0;
+        while (userInput != 5) {
+            System.out.println("\nWhat would you like to do?");
+            System.out.print("\t1. Locate Title\n\t2. Update inventory\n\t3. Check Balance\n\t4. Generate Reports\n\t5. Log Out\nEnter Choice: ");
+            userInput = input.nextInt();
+            if (userInput == 1) {
+                System.out.println("Please enter the name of the book you're trying to locate: ");
+                //query based on book name, show all details about book
 
-                } else if (userInput == 2) {
-                    System.out.println("What would you like to do?\n1. Add to inventory\n2. Delete title\n3. Update title");
-                    //run query based on user input, prob a switch statement is needed, etc.
+            } else if (userInput == 2) {
+                System.out.println("What would you like to do?\n1. Add to inventory\n2. Delete title\n3. Update title");
+                //run query based on user input, prob a switch statement is needed, etc.
 
-                } else if (userInput == 3) {
-                    System.out.println("What would you like to do?\n1. Check user balance\n2. Apply late fee to user");
-                    //depending on the option, could prob accomplish both tasks with one query
+            } else if (userInput == 3) {
+                System.out.println("What would you like to do?\n1. Check user balance\n2. Apply late fee to user");
+                //depending on the option, could prob accomplish both tasks with one query
 
-                } else if (userInput == 4) {
-                    System.out.println("Generating Reports..");
-                    //doesn't state for dates, but could ask for dates. or just print every record that we have, shouldn't
-                    // be too much though
+            } else if (userInput == 4) {
+                System.out.println("Generating Reports..");
+                //doesn't state for dates, but could ask for dates. or just print every record that we have, shouldn't
+                // be too much though
 
-                } else {
-                    System.out.println("Command not recognised");
-                }
+            } else if (userInput == 5){
+                System.out.println("Goodbye!");
+
             } else {
-                System.out.println("Good Bye!");
-                break;
+                System.out.println("Command not recognised");
             }
         }
     }
@@ -138,4 +135,3 @@ public class App {
         }
     }
 }
-
